e128c0415b3e9744d1acc14a23a8cbd34a43c5d0
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-01-22 23:25:21 -0800

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import './App.css';
+import styled, {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+    background-color: #15202b;
+    color: #ffffff;
+  }
+`;
+
+const TestText = styled.p`
+    font-weight: 700;
+`;
 
 const App: React.FC = () => {
-  return (
-    <>
-      Test Test Test
-    </>
-  );
+    return (
+        <>
+            <TestText>Some test text</TestText>
+            <GlobalStyles/>
+        </>
+    );
 };
 
 export default App;
